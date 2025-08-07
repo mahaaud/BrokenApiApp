15581de9648ee7cc4463dfbd73d11e12fdaea67b
Aun - Parit <parit.w@softdebut.com>
2025-08-07 17:30:11 +0700

    Trigger workflow test

--- Controllers/BackendController.cs
+++ Controllers/BackendController.cs
@@ -6,6 +6,13 @@ namespace BrokenApiApp.Controllers
     [Route("[controller]/[action]")]
     public class BackendController : ControllerBase
     {
+        private readonly string _conn = "Server=localhost;Database=Test;User Id=admin;Password=Password123!";
 
+        private readonly ILogger<BackendController> _logger;
+
+        public BackendController(ILogger<BackendController> logger)
+        {
+            _logger = logger;
+        }
     }
 }
